3eb15b2af45d0fe0e2ce313654983b4f6f82755a
Riley Worstell <rcwors02@gmail.com>
2020-05-03 18:25:15 -0400

    Initial Commit

---
+++ RileysTicTacToe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RileysTicTacToe
+//
+//  Created by Riley Worstell on 5/3/20.
+//  Copyright © 2020 Riley Worstell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
